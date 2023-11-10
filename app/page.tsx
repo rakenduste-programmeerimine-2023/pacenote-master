be6f0ee4c2290619b6466dbb62b77046870dcae4
Adrian Käsper <adriank@tlu.ee>
2023-11-10 21:24:12 +0200

    feat: add NewPacenoteForm component

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import NewPacenoteForm from '@/components/NewPacenoteForm';
 import React from 'react';
 
 interface MyComponentProps {
@@ -10,6 +11,7 @@ const MyComponent: React.FC<MyComponentProps> = (props) => {
   return (
     <div>
       <h1>This is the index page of PacenoteMaster</h1>
+      <NewPacenoteForm/>
     </div>
   );
 };
