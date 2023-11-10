cbcb92cfd39220cfe2bbc3055b27269cd30fd26f
Adrian Käsper <adriank@tlu.ee>
2023-11-10 19:35:30 +0200

    feat: add main routes

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,14 @@
 import React from 'react';
 
 interface MyComponentProps {
-  // Define your component props here
+
 }
 
 const MyComponent: React.FC<MyComponentProps> = (props) => {
-  // Add your component logic here
+
 
   return (
     <div>
-      {/* Your component JSX goes here */}
       <h1>This is the index page of PacenoteMaster</h1>
     </div>
   );
