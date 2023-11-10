dd23a37e8c0b3e0ab1cd366abe37cd1aa0aa686d
Adrian Käsper <adriank@tlu.ee>
2023-11-10 22:17:15 +0200

    fix: only one radio button can be selected

--- components/StageEditor/NewPacenoteForm.tsx
+++ components/StageEditor/NewPacenoteForm.tsx
@@ -8,39 +8,39 @@ const NewPacenoteForm = () => {
     <div>
         <form>
             {/* Left turns */}
-            <PacenoteFormComponent type="radio" name="LAcute" value="LAcute" label="Left Acute" />
-            <PacenoteFormComponent type="radio" name="LHairpin" value="LHairpin" label="Left Hairpin" />
-            <PacenoteFormComponent type="radio" name="LSquare" value="LSquare" label="Left Square" />
-            <PacenoteFormComponent type="radio" name="L1" value="L1" label="Left 1" />
-            <PacenoteFormComponent type="radio" name="L2" value="L2" label="Left 2" />
-            <PacenoteFormComponent type="radio" name="L3" value="L3" label="Left 3" />
-            <PacenoteFormComponent type="radio" name="L4" value="L4" label="Left 4" />
-            <PacenoteFormComponent type="radio" name="L5" value="L5" label="Left 5" />
-            <PacenoteFormComponent type="radio" name="L6" value="L6" label="Left 6" />
-            <PacenoteFormComponent type="radio" name="LSlight" value="LSlight" label="Left Slight" />
+            <PacenoteFormComponent type="radio" name="turnGroup" value="LAcute" label="Left Acute" />
+            <PacenoteFormComponent type="radio" name="turnGroup" value="LHairpin" label="Left Hairpin" />
+            <PacenoteFormComponent type="radio" name="turnGroup" value="LSquare" label="Left Square" />
+            <PacenoteFormComponent type="radio" name="turnGroup" value="L1" label="Left 1" />
+            <PacenoteFormComponent type="radio" name="turnGroup" value="L2" label="Left 2" />
+            <PacenoteFormComponent type="radio" name="turnGroup" value="L3" label="Left 3" />
+            <PacenoteFormComponent type="radio" name="turnGroup" value="L4" label="Left 4" />
+            <PacenoteFormComponent type="radio" name="turnGroup" value="L5" label="Left 5" />
+            <PacenoteFormComponent type="radio" name="turnGroup" value="L6" label="Left 6" />
+            <PacenoteFormComponent type="radio" name="turnGroup" value="LSlight" label="Left Slight" />
         
             {/* Right turns */}
-            <PacenoteFormComponent type="radio" name="RAcute" value="RAcute" label="Right Acute" />
-            <PacenoteFormComponent type="radio" name="RHairpin" value="RHairpin" label="Right Hairpin" />
-            <PacenoteFormComponent type="radio" name="RSquare" value="RSquare" label="Right Square" />
-            <PacenoteFormComponent type="radio" name="R1" value="R1" label="Right 1" />
-            <PacenoteFormComponent type="radio" name="R2" value="R2" label="Right 2" />
-            <PacenoteFormComponent type="radio" name="R3" value="R3" label="Right 3" />
-            <PacenoteFormComponent type="radio" name="R4" value="R4" label="Right 4" />
-            <PacenoteFormComponent type="radio" name="R5" value="R5" label="Right 5" />
-            <PacenoteFormComponent type="radio" name="R6" value="R6" label="Right 6" />
-            <PacenoteFormComponent type="radio" name="RSlight" value="RSlight" label="Right Slight" />
+            <PacenoteFormComponent type="radio" name="turnGroup" value="RAcute" label="Right Acute" />
+            <PacenoteFormComponent type="radio" name="turnGroup" value="RHairpin" label="Right Hairpin" />
+            <PacenoteFormComponent type="radio" name="turnGroup" value="RSquare" label="Right Square" />
+            <PacenoteFormComponent type="radio" name="turnGroup" value="R1" label="Right 1" />
+            <PacenoteFormComponent type="radio" name="turnGroup" value="R2" label="Right 2" />
+            <PacenoteFormComponent type="radio" name="turnGroup" value="R3" label="Right 3" />
+            <PacenoteFormComponent type="radio" name="turnGroup" value="R4" label="Right 4" />
+            <PacenoteFormComponent type="radio" name="turnGroup" value="R5" label="Right 5" />
+            <PacenoteFormComponent type="radio" name="turnGroup" value="R6" label="Right 6" />
+            <PacenoteFormComponent type="radio" name="turnGroup" value="RSlight" label="Right Slight" />
         
             {/* Events */}
-            <PacenoteFormComponent type="radio" name="Straight" value="Straight" label="Straight" />
-            <PacenoteFormComponent type="radio" name="LChicane" value="LChicane" label="Left chicane" />
-            <PacenoteFormComponent type="radio" name="RChicane" value="RChicane" label="Right chicane" />
-            <PacenoteFormComponent type="radio" name="Dip" value="Dip" label="Dip" />
-            <PacenoteFormComponent type="radio" name="Bump" value="Bump" label="Bump" />
-            <PacenoteFormComponent type="radio" name="Crest" value="Crest" label="Crest" />
-            <PacenoteFormComponent type="radio" name="Jump" value="Jump" label="Jump" />
-            <PacenoteFormComponent type="radio" name="Twisting" value="Twisting" label="Twisting" />
-            <PacenoteFormComponent type="radio" name="Watersplash" value="Watersplash" label="Watersplash" />
+            <PacenoteFormComponent type="radio" name="turnGroup" value="Straight" label="Straight" />
+            <PacenoteFormComponent type="radio" name="turnGroup" value="LChicane" label="Left chicane" />
+            <PacenoteFormComponent type="radio" name="turnGroup" value="RChicane" label="Right chicane" />
+            <PacenoteFormComponent type="radio" name="turnGroup" value="Dip" label="Dip" />
+            <PacenoteFormComponent type="radio" name="turnGroup" value="Bump" label="Bump" />
+            <PacenoteFormComponent type="radio" name="turnGroup" value="Crest" label="Crest" />
+            <PacenoteFormComponent type="radio" name="turnGroup" value="Jump" label="Jump" />
+            <PacenoteFormComponent type="radio" name="turnGroup" value="Twisting" label="Twisting" />
+            <PacenoteFormComponent type="radio" name="turnGroup" value="Watersplash" label="Watersplash" />
         
             {/* Notes */}
             <label>
