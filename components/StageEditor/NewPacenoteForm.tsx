6c5269352efb4ca77647f2aa1cfc127511bca540
Adrian Käsper <adriank@tlu.ee>
2023-11-11 19:25:02 +0200

    fix: correct PacenoteList elements

--- components/StageEditor/NewPacenoteForm.tsx
+++ components/StageEditor/NewPacenoteForm.tsx
@@ -4,7 +4,7 @@ import React, { useState } from 'react';
 
 const NewPacenoteForm = () => {
     const [formData, setFormData] = useState({
-      turnGroup: '',
+      action: '',
       notes: '',
       Cut: false,
       DontCut: false,
@@ -24,7 +24,7 @@ const NewPacenoteForm = () => {
     
 
       setFormData({
-        turnGroup: '',
+        action: '',
         notes: '',
         Cut: false,
         DontCut: false,
@@ -39,39 +39,39 @@ const NewPacenoteForm = () => {
     <div>
         <form onSubmit={handleSubmit}>
             {/* Left turns */}
-            <label><input type="radio" name="turnGroup" value="LAcute" checked={formData.turnGroup === 'LAcute'} onChange={(e) => setFormData({ ...formData, turnGroup: e.target.value })}/> Left Acute</label>
-            <label><input type="radio" name="turnGroup" value="LHairpin" checked={formData.turnGroup === 'LHairpin'} onChange={(e) => setFormData({ ...formData, turnGroup: e.target.value })}/> Left Hairpin</label>
-            <label><input type="radio" name="turnGroup" value="LSquare" checked={formData.turnGroup === 'LSquare'} onChange={(e) => setFormData({ ...formData, turnGroup: e.target.value })}/> Left Square</label>
-            <label><input type="radio" name="turnGroup" value="L1" checked={formData.turnGroup === 'L1'} onChange={(e) => setFormData({ ...formData, turnGroup: e.target.value })}/> Left 1</label>
-            <label><input type="radio" name="turnGroup" value="L2" checked={formData.turnGroup === 'L2'} onChange={(e) => setFormData({ ...formData, turnGroup: e.target.value })}/> Left 2</label>
-            <label><input type="radio" name="turnGroup" value="L3" checked={formData.turnGroup === 'L3'} onChange={(e) => setFormData({ ...formData, turnGroup: e.target.value })}/> Left 3</label>
-            <label><input type="radio" name="turnGroup" value="L4" checked={formData.turnGroup === 'L4'} onChange={(e) => setFormData({ ...formData, turnGroup: e.target.value })}/> Left 4</label>
-            <label><input type="radio" name="turnGroup" value="L5" checked={formData.turnGroup === 'L5'} onChange={(e) => setFormData({ ...formData, turnGroup: e.target.value })}/> Left 5</label>
-            <label><input type="radio" name="turnGroup" value="L6" checked={formData.turnGroup === 'L6'} onChange={(e) => setFormData({ ...formData, turnGroup: e.target.value })}/> Left 6</label>
-            <label><input type="radio" name="turnGroup" value="LSlight" checked={formData.turnGroup === 'LSlight'} onChange={(e) => setFormData({ ...formData, turnGroup: e.target.value })}/> Left Slight</label>
+            <label><input type="radio" name="action" value="LAcute" checked={formData.action === 'LAcute'} onChange={(e) => setFormData({ ...formData, action: e.target.value })}/> Left Acute</label>
+            <label><input type="radio" name="action" value="LHairpin" checked={formData.action === 'LHairpin'} onChange={(e) => setFormData({ ...formData, action: e.target.value })}/> Left Hairpin</label>
+            <label><input type="radio" name="action" value="LSquare" checked={formData.action === 'LSquare'} onChange={(e) => setFormData({ ...formData, action: e.target.value })}/> Left Square</label>
+            <label><input type="radio" name="action" value="L1" checked={formData.action === 'L1'} onChange={(e) => setFormData({ ...formData, action: e.target.value })}/> Left 1</label>
+            <label><input type="radio" name="action" value="L2" checked={formData.action === 'L2'} onChange={(e) => setFormData({ ...formData, action: e.target.value })}/> Left 2</label>
+            <label><input type="radio" name="action" value="L3" checked={formData.action === 'L3'} onChange={(e) => setFormData({ ...formData, action: e.target.value })}/> Left 3</label>
+            <label><input type="radio" name="action" value="L4" checked={formData.action === 'L4'} onChange={(e) => setFormData({ ...formData, action: e.target.value })}/> Left 4</label>
+            <label><input type="radio" name="action" value="L5" checked={formData.action === 'L5'} onChange={(e) => setFormData({ ...formData, action: e.target.value })}/> Left 5</label>
+            <label><input type="radio" name="action" value="L6" checked={formData.action === 'L6'} onChange={(e) => setFormData({ ...formData, action: e.target.value })}/> Left 6</label>
+            <label><input type="radio" name="action" value="LSlight" checked={formData.action === 'LSlight'} onChange={(e) => setFormData({ ...formData, action: e.target.value })}/> Left Slight</label>
         
             {/* Right turns */}
-            <label><input type="radio" name="turnGroup" value="RAcute" checked={formData.turnGroup === 'RAcute'} onChange={(e) => setFormData({ ...formData, turnGroup: e.target.value })}/> Right Acute</label>
-            <label><input type="radio" name="turnGroup" value="RHairpin" checked={formData.turnGroup === 'RHairpin'} onChange={(e) => setFormData({ ...formData, turnGroup: e.target.value })}/> Right Hairpin</label>
-            <label><input type="radio" name="turnGroup" value="RSquare" checked={formData.turnGroup === 'RSquare'} onChange={(e) => setFormData({ ...formData, turnGroup: e.target.value })}/> Right Square</label>
-            <label><input type="radio" name="turnGroup" value="R1" checked={formData.turnGroup === 'R1'} onChange={(e) => setFormData({ ...formData, turnGroup: e.target.value })}/> Right 1</label>
-            <label><input type="radio" name="turnGroup" value="R2" checked={formData.turnGroup === 'R2'} onChange={(e) => setFormData({ ...formData, turnGroup: e.target.value })}/> Right 2</label>
-            <label><input type="radio" name="turnGroup" value="R3" checked={formData.turnGroup === 'R3'} onChange={(e) => setFormData({ ...formData, turnGroup: e.target.value })}/> Right 3</label>
-            <label><input type="radio" name="turnGroup" value="R4" checked={formData.turnGroup === 'R4'} onChange={(e) => setFormData({ ...formData, turnGroup: e.target.value })}/> Right 4</label>
-            <label><input type="radio" name="turnGroup" value="R5" checked={formData.turnGroup === 'R5'} onChange={(e) => setFormData({ ...formData, turnGroup: e.target.value })}/> Right 5</label>
-            <label><input type="radio" name="turnGroup" value="R6" checked={formData.turnGroup === 'R6'} onChange={(e) => setFormData({ ...formData, turnGroup: e.target.value })}/> Right 6</label>
-            <label><input type="radio" name="turnGroup" value="RSlight" checked={formData.turnGroup === 'RSlight'} onChange={(e) => setFormData({ ...formData, turnGroup: e.target.value })}/> Right Slight</label>
+            <label><input type="radio" name="action" value="RAcute" checked={formData.action === 'RAcute'} onChange={(e) => setFormData({ ...formData, action: e.target.value })}/> Right Acute</label>
+            <label><input type="radio" name="action" value="RHairpin" checked={formData.action === 'RHairpin'} onChange={(e) => setFormData({ ...formData, action: e.target.value })}/> Right Hairpin</label>
+            <label><input type="radio" name="action" value="RSquare" checked={formData.action === 'RSquare'} onChange={(e) => setFormData({ ...formData, action: e.target.value })}/> Right Square</label>
+            <label><input type="radio" name="action" value="R1" checked={formData.action === 'R1'} onChange={(e) => setFormData({ ...formData, action: e.target.value })}/> Right 1</label>
+            <label><input type="radio" name="action" value="R2" checked={formData.action === 'R2'} onChange={(e) => setFormData({ ...formData, action: e.target.value })}/> Right 2</label>
+            <label><input type="radio" name="action" value="R3" checked={formData.action === 'R3'} onChange={(e) => setFormData({ ...formData, action: e.target.value })}/> Right 3</label>
+            <label><input type="radio" name="action" value="R4" checked={formData.action === 'R4'} onChange={(e) => setFormData({ ...formData, action: e.target.value })}/> Right 4</label>
+            <label><input type="radio" name="action" value="R5" checked={formData.action === 'R5'} onChange={(e) => setFormData({ ...formData, action: e.target.value })}/> Right 5</label>
+            <label><input type="radio" name="action" value="R6" checked={formData.action === 'R6'} onChange={(e) => setFormData({ ...formData, action: e.target.value })}/> Right 6</label>
+            <label><input type="radio" name="action" value="RSlight" checked={formData.action === 'RSlight'} onChange={(e) => setFormData({ ...formData, action: e.target.value })}/> Right Slight</label>
         
             {/* Events */}
-            <label><input type="radio" name="turnGroup" value="Straight" checked={formData.turnGroup === 'Straight'} onChange={(e) => setFormData({ ...formData, turnGroup: e.target.value })}/> Straight</label>
-            <label><input type="radio" name="turnGroup" value="LChicane" checked={formData.turnGroup === 'LChicane'} onChange={(e) => setFormData({ ...formData, turnGroup: e.target.value })}/> Left chicane</label>
-            <label><input type="radio" name="turnGroup" value="RChicane" checked={formData.turnGroup === 'RChicane'} onChange={(e) => setFormData({ ...formData, turnGroup: e.target.value })}/> Right chicane</label>
-            <label><input type="radio" name="turnGroup" value="Dip" checked={formData.turnGroup === 'Dip'} onChange={(e) => setFormData({ ...formData, turnGroup: e.target.value })}/> Dip</label>
-            <label><input type="radio" name="turnGroup" value="Bump" checked={formData.turnGroup === 'Bump'} onChange={(e) => setFormData({ ...formData, turnGroup: e.target.value })}/> Bump</label>
-            <label><input type="radio" name="turnGroup" value="Crest" checked={formData.turnGroup === 'Crest'} onChange={(e) => setFormData({ ...formData, turnGroup: e.target.value })}/> Crest</label>
-            <label><input type="radio" name="turnGroup" value="Jump" checked={formData.turnGroup === 'Jump'} onChange={(e) => setFormData({ ...formData, turnGroup: e.target.value })}/> Jump</label>
-            <label><input type="radio" name="turnGroup" value="Twisting" checked={formData.turnGroup === 'Twisting'} onChange={(e) => setFormData({ ...formData, turnGroup: e.target.value })}/> Twisting</label>
-            <label><input type="radio" name="turnGroup" value="Watersplash" checked={formData.turnGroup === 'Watersplash'} onChange={(e) => setFormData({ ...formData, turnGroup: e.target.value })}/> Watersplash</label>
+            <label><input type="radio" name="action" value="Straight" checked={formData.action === 'Straight'} onChange={(e) => setFormData({ ...formData, action: e.target.value })}/> Straight</label>
+            <label><input type="radio" name="action" value="LChicane" checked={formData.action === 'LChicane'} onChange={(e) => setFormData({ ...formData, action: e.target.value })}/> Left chicane</label>
+            <label><input type="radio" name="action" value="RChicane" checked={formData.action === 'RChicane'} onChange={(e) => setFormData({ ...formData, action: e.target.value })}/> Right chicane</label>
+            <label><input type="radio" name="action" value="Dip" checked={formData.action === 'Dip'} onChange={(e) => setFormData({ ...formData, action: e.target.value })}/> Dip</label>
+            <label><input type="radio" name="action" value="Bump" checked={formData.action === 'Bump'} onChange={(e) => setFormData({ ...formData, action: e.target.value })}/> Bump</label>
+            <label><input type="radio" name="action" value="Crest" checked={formData.action === 'Crest'} onChange={(e) => setFormData({ ...formData, action: e.target.value })}/> Crest</label>
+            <label><input type="radio" name="action" value="Jump" checked={formData.action === 'Jump'} onChange={(e) => setFormData({ ...formData, action: e.target.value })}/> Jump</label>
+            <label><input type="radio" name="action" value="Twisting" checked={formData.action === 'Twisting'} onChange={(e) => setFormData({ ...formData, action: e.target.value })}/> Twisting</label>
+            <label><input type="radio" name="action" value="Watersplash" checked={formData.action === 'Watersplash'} onChange={(e) => setFormData({ ...formData, action: e.target.value })}/> Watersplash</label>
 
             {/* Notes */}
             <label>Additional notes: <input type="text" name="notes"  value={formData.notes} onChange={(e) => setFormData({ ...formData, notes: e.target.value })} /></label>
