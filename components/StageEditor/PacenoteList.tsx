9bd63ccf81db3b0d0ebea000c60810c59ccefb15
Adrian Käsper <adriank@tlu.ee>
2023-11-11 19:25:02 +0200

    fix: correct PacenoteList elements

--- components/StageEditor/PacenoteList.tsx
+++ components/StageEditor/PacenoteList.tsx
@@ -1,46 +1,87 @@
 "use client";
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 const DraggableList = () => {
-  const [items, setItems] = useState([
-    'Item 1',
-    'Item 2',
-    'Item 3',
-    // Add more items as needed
-  ]);
+    const [items, setItems] = useState([
+        {
+            action: "R6",
+            Cut: true,
+            DontCut: false,
+            Caution: false,
+            Danger: false,
+            Widens: false,
+            Tightens: true,
+            notes: "Keep in"
+        },
+        {
+            action: "L2",
+            Cut: false,
+            DontCut: true,
+            Caution: false,
+            Danger: false,
+            Widens: true,
+            Tightens: false,
+            notes: "Tree inside"
+        },
+        {
+            action: "L4",
+            Cut: false,
+            DontCut: false,
+            Caution: true,
+            Danger: true,
+            Widens: false,
+            Tightens: false,
+            notes: "Uneven terrain"
+        }
+    ]);
 
-  const handleDragStart = (e: React.DragEvent<HTMLLIElement>, index: number) => {
-    e.dataTransfer.setData('text/plain', index.toString()); // Convert index to string
-  };
+    const handleDragStart = (
+        e: React.DragEvent<HTMLLIElement>,
+        index: number
+    ) => {
+        e.dataTransfer.setData("text/plain", index.toString()); // Convert index to string
+    };
 
-  const handleDragOver = (e: React.DragEvent<HTMLLIElement>) => {
-    e.preventDefault();
-  };
+    const handleDragOver = (e: React.DragEvent<HTMLLIElement>) => {
+        e.preventDefault();
+    };
 
-  const handleDrop = (e: React.DragEvent<HTMLLIElement>, newIndex: number) => {
-    e.preventDefault();
-    const draggedIndex = parseInt(e.dataTransfer.getData('text/plain'), 10);
-    const newItems = [...items];
-    const [draggedItem] = newItems.splice(draggedIndex, 1);
-    newItems.splice(newIndex, 0, draggedItem);
-    setItems(newItems);
-  };
+    const handleDrop = (
+        e: React.DragEvent<HTMLLIElement>,
+        newIndex: number
+    ) => {
+        e.preventDefault();
+        const draggedIndex = parseInt(e.dataTransfer.getData("text/plain"), 10);
+        const newItems = [...items];
+        const [draggedItem] = newItems.splice(draggedIndex, 1);
+        newItems.splice(newIndex, 0, draggedItem);
+        setItems(newItems);
+    };
 
-  return (
-    <ul>
-      {items.map((item, index) => (
-        <li
-          key={index}
-          draggable
-          onDragStart={(e) => handleDragStart(e, index)}
-          onDragOver={handleDragOver}
-          onDrop={(e) => handleDrop(e, index)}
-        >
-          {item}
-        </li>
-      ))}
-    </ul>
-  );
+    return (
+        <div>
+            <ul>
+                {items.map((item, index) => (
+                    <li
+                        key={index}
+                        draggable
+                        onDragStart={(e) => handleDragStart(e, index)}
+                        onDragOver={handleDragOver}
+                        onDrop={(e) => handleDrop(e, index)}
+                    >
+                        <span>{item.action} </span>
+                        <input type="checkbox" checked={item.Cut} readOnly /> 
+                        <input type="checkbox" checked={item.DontCut} readOnly /> 
+                        <input type="checkbox" checked={item.Caution} readOnly /> 
+                        <input type="checkbox" checked={item.Danger} readOnly /> 
+                        <input type="checkbox" checked={item.Widens} readOnly /> 
+                        <input type="checkbox" checked={item.Tightens} readOnly /> 
+                        <span>{item.notes} </span>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
 };
 
 export default DraggableList;
