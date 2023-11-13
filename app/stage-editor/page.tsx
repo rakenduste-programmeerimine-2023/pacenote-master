63e643361d3a86575b137a2ccb40fda29d2b6444
Adrian Käsper <adriank@tlu.ee>
2023-11-13 23:57:14 +0200

    refactor: various code cleanup in stage-editor

--- app/stage-editor/page.tsx
+++ app/stage-editor/page.tsx
@@ -1,31 +1,29 @@
-"use client";
+"use client"
 import NewPacenoteForm from "@/components/StageEditor/NewPacenoteForm"
 import PacenoteList from "@/components/StageEditor/PacenoteList"
-import { SaveStageToDB } from "@/components/backend/StageEditor/beStageEditor";
+import { SaveStageToDB } from "@/components/backend/StageEditor/beStageEditor"
+import React, { useState, useEffect } from "react"
 import Test from "@/components/backend/StageEditor/test"
-import React, { useState, useEffect } from "react";
 
 interface StageEditorProps {}
 
 const StageEditor: React.FC<StageEditorProps> = props => {
+    /*const [testData, setTestData] = useState<string | null>(null)
 
-  const [testData, setTestData] = useState<string | null>(null);
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const testResult = await Test()
+                console.log("Test result: ", testResult)
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const testResult = await Test();
-        console.log("Test result: ", testResult);
+                setTestData(testResult)
+            } catch (error) {
+                console.error("Error fetching test data:", error)
+            }
+        }
 
-        setTestData(testResult);
-      } catch (error) {
-        console.error('Error fetching test data:', error);
-      }
-    };
-
-    fetchData();
-  }, []);
-
+        fetchData()
+    }, [])*/
 
     const [pacenotes, setPacenotes] = useState([
         {
@@ -37,55 +35,40 @@ const StageEditor: React.FC<StageEditorProps> = props => {
             Widens: false,
             Tightens: true,
             Notes: "Keep in"
-        }]);
+        }
+    ])
 
-    const handleFormSubmit = (formData: any) => {
-        const newPacenotes = AddNewPacenoteToList(
-          formData.action,
-          formData.Cut,
-          formData.DontCut,
-          formData.Caution,
-          formData.Danger,
-          formData.Widens,
-          formData.Tightens,
-          formData.notes
-        );
-        setPacenotes(newPacenotes);
-      };
+    const HandleAddNewPacenoteToList = (newPacenoteFromForm: any) => {
+      setPacenotes(pacenotes => [
+          ...pacenotes,
+          {
+              Action: newPacenoteFromForm.Action,
+              Cut: newPacenoteFromForm.Cut,
+              DontCut: newPacenoteFromForm.DontCut,
+              Caution: newPacenoteFromForm.Caution,
+              Danger: newPacenoteFromForm.Danger,
+              Widens: newPacenoteFromForm.Widens,
+              Tightens: newPacenoteFromForm.Tightens,
+              Notes: newPacenoteFromForm.Notes
+          }
+      ]);
+  }
 
-      const handleFinishButtonClick = async () => {
+    const HandleFinishButtonClick = async () => {
         SaveStageToDB(pacenotes, 1)
-      };
-
-
-        // Function to add new pacenote
-        const AddNewPacenoteToList = (action: string, cut: boolean, dontCut: boolean, caution: boolean, danger: boolean, widens: boolean, tightens: boolean, notes: string) => {
-            const newPacenotes = [
-                ...pacenotes,
-                {
-                    Action: action,
-                    Cut: cut,
-                    DontCut: dontCut,
-                    Caution: caution,
-                    Danger: danger,
-                    Widens: widens,
-                    Tightens: tightens,
-                    Notes: notes,
-                },
-            ];
-            return newPacenotes;
-        };
+    }
 
     return (
         <div>
             <h1>Stage editor</h1>
-            <h1>Your Page</h1>
-            {testData && (
+            {/*{testData && (
               <pre>{testData}</pre>
-            )}
-            <PacenoteList pacenotes={pacenotes} setPacenotes={setPacenotes} />
-            <NewPacenoteForm  onSubmit={handleFormSubmit} />
-            <button type="submit" onClick={handleFinishButtonClick}>Finish</button>
+            )}*/}
+            <PacenoteList pacenotes={pacenotes} setPacenotes={setPacenotes}/>
+            <NewPacenoteForm onSubmit={HandleAddNewPacenoteToList} />
+            <button type="submit" onClick={HandleFinishButtonClick}>
+                Finish
+            </button>
         </div>
     )
 }
