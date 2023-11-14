2541e8ce6b6d5705e53fd3cddd7c3ed9cdcbf18b
sandersims <sander02@tlu.ee>
2023-11-14 16:54:24 +0200

    feat: add navBar

---
+++ app/home/page.tsx
@@ -0,0 +1,15 @@
+'use client'
+import React from 'react';
+import ResponsiveAppBar from '@/components/Header';
+
+function HomePage() {
+  
+
+  return (
+    <div>
+      <ResponsiveAppBar />
+    </div>
+  );
+}
+
+export default HomePage;
