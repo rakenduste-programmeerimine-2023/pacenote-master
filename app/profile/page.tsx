3df14151814d0922c61679a39d5b6b83bb3a1c62
Adrian Käsper <adriank@tlu.ee>
2023-12-01 16:10:57 +0200

    feat: add Profile page

---
+++ app/profile/page.tsx
@@ -0,0 +1,65 @@
+"use client"
+import React, { useState, useEffect } from "react";
+import { useSearchParams } from 'next/navigation';
+import { LoadProfileData } from "@/components/backend/Profile/beProfile";
+
+interface ProfileProps {}
+
+export const Profile: React.FC<ProfileProps> = props => {
+    const searchParams = useSearchParams();
+    let profileID = searchParams.get('id') || null;
+    const [profileData, setProfileData] = useState<any>(null);
+    const [isLoading, setIsLoading] = useState(true);
+
+    useEffect(() => {
+        document.title = 'Stage Editor';
+        const fetchData = async () => {
+            try {
+                let result = await LoadProfileData(profileID);
+                if (result !== null) {
+                    console.log("Profile Data:", result);
+                    setProfileData(result);
+                } else {
+                    console.error("Error loading profile data");
+                }
+            } catch (error) {
+                console.error("Error fetching profile data:", error);
+            } finally {
+                setIsLoading(false);
+            }
+        };
+
+        fetchData();
+    }, [profileID]);
+
+    useEffect(() => {
+        // Log after the state has been updated
+        console.log("profileData: ", profileData);
+    
+        // Update the document title here
+        document.title = profileData ? `Profile: ${profileData.avatar}` : 'Stage Editor';
+    }, [profileData]);
+
+    console.log("profileID: ", profileID);
+    
+    // Add a log statement for debugging when the component renders
+    console.log("Component rendered with profileData: ", profileData);
+
+    if (isLoading) {
+        return <p>Loading...</p>;
+    }
+
+    if (!profileData) {
+        return <p>Error loading profile data</p>;
+    }
+
+    return (
+        <div>
+            <p>Profile: {profileData[0].username}</p>
+            <p>Description: {profileData[0].description}</p>
+            <img src={profileData && profileData.length > 0 ? profileData[0].avatarURL.data.publicUrl : ""} alt="Avatar" />
+        </div>
+    );
+};
+
+export default Profile;
---
+++ components/backend/Profile/beProfile.ts
@@ -0,0 +1,82 @@
+"use server"
+import { createClient } from "@/utils/supabase/server"
+import { cookies } from "next/headers"
+
+/*export const LoadProfileData = async (profileID: string | null) => {
+    if (profileID === null) {
+        console.error("Invalid profile ID");
+        return null;
+    }
+    const cookieStore = cookies();
+    const supabase = createClient(cookieStore);
+    const {
+        data: { user }
+    } = await supabase.auth.getUser();
+    
+    let userData;
+
+    if (user) {
+        const { data, error } = await supabase
+            .from("profiles")
+            .select("*")
+            .eq("user_id", profileID);
+
+        if (error) {
+            console.error("Error loading user data:", error);
+            return null;
+        }
+
+        userData = data;
+    } else {
+        console.error("No signed-in user");
+    }
+
+    return userData;
+};*/
+
+
+
+
+export const LoadProfileData = async (profileID: string | null) => {
+    if (profileID === null) {
+        console.error("Invalid profile ID");
+        return null;
+    }
+
+    const cookieStore = cookies();
+    const supabase = createClient(cookieStore);
+    const {
+        data: { user }
+    } = await supabase.auth.getUser();
+
+    let userData;
+
+    if (user) {
+        const { data, error } = await supabase
+            .from("profiles")
+            .select("*")
+            .eq("user_id", profileID);
+
+        if (error) {
+            console.error("Error loading user data:", error);
+            return null;
+        }
+
+        userData = data;
+
+        // Fetch image URL and add it to the user data
+        if (userData && userData.length > 0) {
+            const avatarURL = await supabase
+                .storage
+                .from('Avatars')
+                .getPublicUrl(`${userData[0].avatar}`);
+            
+            // Add the avatar URL to the user data
+            userData[0].avatarURL = avatarURL;
+        }
+    } else {
+        console.error("No signed-in user");
+    }
+
+    return userData;
+};
